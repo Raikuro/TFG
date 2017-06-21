10d3172f77341d9e573477def7a5044228e36fff
Raikuro <jugragu22@gmail.com>
2017-06-21 18:05:31 +0200

    Handling bd disconnection

--- backend/src/core/mysqlConnection/mysqlConnection.js
+++ backend/src/core/mysqlConnection/mysqlConnection.js
@@ -1,4 +1,26 @@
 let mysql = require('mysql')
+/*
 let connection = mysql.createConnection(require('../../../config/database'))
 connection.connect()
 module.exports = connection
+*/
+handleDisconnect()
+function handleDisconnect () {
+  let connection = mysql.createConnection(require('../../../config/database'))
+  connection.connect((err) => {
+    if (err) {
+      console.log('error when connecting to db:', err)
+      setTimeout(handleDisconnect, 2000)
+    }
+  })
+
+  connection.on('error', (err) => {
+    console.log('db error', err)
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect()
+    } else { throw err }
+  })
+  module.exports = connection
+}
+
+
